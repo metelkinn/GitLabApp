710d56760f6aa51032f938aa5bad3841184bc16b
женя <@METEKIN-ZHENYA->
2024-06-16 13:44:55 +0300

    Добавьте файлы проекта.

---
+++ GitLabApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double result = 2 * Math.Sin(x) + 3 * Math.Cos(x);
+
+        Console.WriteLine("Result: " + result);
+    }
+}
